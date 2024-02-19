f76a73c6e23e16396211a04d737c581a10730ffc
arrouf123 <arrouf97531@gmail.com>
2024-02-19 21:26:09 +0600

    last commit

--- javascriptss/a1.js
+++ javascriptss/a1.js
@@ -14,6 +14,7 @@ document.getElementById('A1').addEventListener('click',function(){
     // visibleCouponButton()
     // visibleMakePurchaseBtn()
     console.log(previousTotalPrice)
+
    
 })
 
@@ -36,6 +37,177 @@ document.getElementById('A2').addEventListener('click',function(){
 // for a3
 
 document.getElementById('A3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for a4
+
+document.getElementById('A4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for B1
+
+document.getElementById('B1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for B1
+
+document.getElementById('B2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for B3
+
+document.getElementById('B3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+// for B4
+
+
+
+
+
+document.getElementById('B4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+// for C1
+
+document.getElementById('C1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+// for C2
+
+document.getElementById('C2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+// for C3
+
+document.getElementById('C3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for C4
+
+document.getElementById('C4').addEventListener('click',function(){
     const ticketsPrice = 550;
     const previousTotalPrice = document.getElementById('total-price').innerText
     const currentPriceInt = parseInt(previousTotalPrice);
---
+++ javascriptss/b1.js
@@ -0,0 +1,546 @@
+// for D1
+
+document.getElementById('D1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for D2
+
+document.getElementById('D2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for D3
+
+document.getElementById('D3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+// for D4
+
+document.getElementById('D4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+// for E1
+
+document.getElementById('E1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+//  for E2
+
+document.getElementById('E2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for E3
+
+document.getElementById('E3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for E4
+
+document.getElementById('E4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+
+//  for F1
+
+document.getElementById('F1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for F2
+
+document.getElementById('F2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for F3
+
+document.getElementById('F3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for F4
+
+document.getElementById('F4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for G1
+
+document.getElementById('G1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for G2
+
+document.getElementById('G2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for G3
+
+document.getElementById('G3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+//  for G4
+
+document.getElementById('G4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+//  for H1
+
+document.getElementById('H1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for H2
+
+document.getElementById('H2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+//  for H3
+
+document.getElementById('H3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for H4
+
+document.getElementById('H4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+//  for I1
+
+document.getElementById('I1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for I2
+
+document.getElementById('I2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for I3
+
+document.getElementById('I3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for I4
+
+document.getElementById('I4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for J1
+
+document.getElementById('J1').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for J2
+
+document.getElementById('J2').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for J3
+
+document.getElementById('J3').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
+
+
+
+//  for J4
+
+document.getElementById('J4').addEventListener('click',function(){
+    const ticketsPrice = 550;
+    const previousTotalPrice = document.getElementById('total-price').innerText
+    const currentPriceInt = parseInt(previousTotalPrice);
+    const currentPrice = currentPriceInt + ticketsPrice
+    
+    // setElementById('total-price',currentPrice)
+    console.log(currentPriceInt);
+    document.getElementById('total-price').innerText = currentPrice
+
+    // EntryElement('K. Accessories')
+    // visibleCouponButton()
+    // visibleMakePurchaseBtn()
+    console.log(previousTotalPrice)
+})
